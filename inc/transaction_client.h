ea807759fdce49c76582eda38dd952f0f2849aed
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-12-15 20:41:01 +0100

    added basic structure to the transaction client

--- inc/transaction_client.h
+++ inc/transaction_client.h
@@ -1,6 +1,22 @@
 #ifndef TRANSACTION_CLINET_H
 #define TRANSACTION_CLINET_H
 
+#include<dstring.h>
+#include<queue.h>
+#include<hashmap.h>
+
 #include<client.h>
+
+// the transaction_client is responsible for executing the transactions made on a connection with multiple threads one for each connection
+
+// this function will open an additional of a number of connections, for doing the transactions
+void add_connections(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, int number_of_connections);
+
+// the add_transaction_to function adds a transaction to be performed on a queue
+// the transaction, can be performed by any of the connection, specified by tuple => (protocol, ip, port)
+void add_transaction_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, void (*transaction)(int fd));
+
+// this function will close all the connections to the specific
+void close_all_connections_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT);
 
 #endif
--- src/transaction_client.c
+++ src/transaction_client.c
@@ -1,1 +1,41 @@
 #include<transaction_client.h>
+
+// this is a global hashmap, which will store a mapping from
+// [ tcp or udp or any other protocol ] + [ip_address of the machine] + [port on the machine to talk to] -> queue_shared among the connections for the transactions 
+hashmap* connection_to_transaction_queue = NULL;
+
+void intialize_transaction_client()
+{
+	// todo
+}
+
+// converts "tcp://127.0.0.1:80" to a randon hash number
+unsigned long long int connection_hash(dstring* connection_identifier)
+{
+	// todo
+	return 0;
+}
+
+// returns things like "tcp://127.0.0.1:80"
+void get_connection_identifier(dstring* identifer, sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT)
+{
+	// todo
+}
+
+
+// below are the implementation to the exposed functions, that can be used to actually create connections, add transactions to the connection queues and close the connections
+
+void add_connections(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, int number_of_connections)
+{
+	// todo
+}
+
+void add_transaction_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, void (*transaction)(int fd))
+{
+	// todo
+}
+
+void close_all_connections_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT)
+{
+	// todo
+}

70a04b2d7ca00c3d4317e98e84600978e498abde
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-12-17 00:05:24 +0100

    removed binary files

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -2,9 +2,9 @@ cd ..
 make clean all
 cd test
 echo "compiling server"
-gcc test_server.c -o server -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
+gcc test_server.c -o server.out -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
 echo "compiling client"
-gcc test_client.c -o client -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
+gcc test_client.c -o client.out -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
 echo "compiling transaction_client"
-gcc test_transaction_client.c -o tclient -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
+gcc test_transaction_client.c -o tclient.out -I../inc -I../../BoomPar/inc -I../../cutlery/inc -L../bin -L../../BoomPar/bin -L../../cutlery/bin -lconnman -lboompar -lcutlery
 echo "compiling done"

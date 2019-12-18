5a7d188bc5ba53670e69e43550078a06667c4ff8
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-12-18 20:25:51 +0100

    revamp for the executor service

--- inc/transaction_client.h
+++ inc/transaction_client.h
@@ -7,26 +7,47 @@
 
 #include<rwlock.h>
 
+#include<executor.h>
+
 #include<client.h>
 
 #include<job.h>
 
-// the transaction_client is responsible for executing the transactions made on a connection with multiple threads one for each connection
+// the connection_group is responsible for executing the transactions to be made on a uniquely identified connection type with multiple threads one for each connection
+// For this purpose the connection_group takes as input the executor, the connection group is not responsible for handling the executor, it will work on whatever shared executor, you provide it with
+typedef struct connection_group connection_group;
+struct connection_group
+{
+	executor* transaction_executor;
 
-// yopu have to first call this function, to use the transaction client
-void intialize_transaction_client();
+	hashmap* thread_to_file_discriptor;
+	rwlock* thread_to_file_discriptor_lock;
 
-// this function will open an additional of a number of connections, for doing the transactions
-void add_connections(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, int number_of_connections);
+	// specifies if the protocol is tcp or ip
+	int TRANSMISSION_PROTOCOL_TYPE;
+
+	// ADDRESS_FAMILY specifies whether, it is an ipv4 or ipv6 connection
+	sa_family_t ADDRESS_FAMILY; // AF_INET or AF_INET
+
+	// ip address as a 32 bit integer, in host byte order
+	uint32_t SERVER_ADDRESS;
+
+	// the port on which the server is running
+	uint16_t PORT;
+};
+
+
+// this function will take an executor service as input parameter, each of whose threads, are responsible for doing the transactions on their individual connections
+// unless their connections break, the connection persists across the transactions
+// the transaction_executor, that you pass in here as input parameter has to be solely maintained by you
+connection_group* get_connection_group(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, executor* transaction_executor);
 
 // the add_transaction_to function adds a transaction to be performed on a queue
 // the transaction, can be performed by any of the connection, specified by tuple => (protocol, ip, port)
-void add_transaction_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, void (*transaction)(int fd));
+void add_transaction_to_connection_group(connection_group* conn_grp_p, void (*transaction)(int fd));
 
-// this function will close all the connections to the specific
-void close_all_connections_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT);
-
-// to close all the connections and destroy all the resources occupied by the transaction_client
-void destroy_transaction_client();
+// this function will only delete the resources occupied by the connection_group
+// this function must be called only after all the jobs in the transaction_executor have been completed
+void delete_connection_group(connection_group* conn_grp_p);
 
 #endif
--- src/transaction_client.c
+++ src/transaction_client.c
@@ -7,10 +7,10 @@ unsigned long long int connection_hash(dstring* connection_identifier)
 }
 
 // returns things like "tcp://127.0.0.1:80"
-void get_connection_identifier(dstring* identifer, sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT)
+void get_connection_group_identifier(dstring* identifer, connection_group* conn_grp_p)
 {
 	make_dstring_empty(identifer);
-	switch(TRANSMISSION_PROTOCOL_TYPE)
+	switch(conn_grp_p->TRANSMISSION_PROTOCOL_TYPE)
 	{
 		case SOCK_STREAM :
 		{
@@ -29,7 +29,7 @@ void get_connection_identifier(dstring* identifer, sa_family_t ADDRESS_FAMILY, i
 	for(int i = 3; i >= 0; i--)
 	{
 		char temp[10] = "";
-		uint32_t ip_part_dec = (SERVER_ADDRESS >> (i * 8)) & (0x000000ff);
+		uint32_t ip_part_dec = (conn_grp_p->SERVER_ADDRESS >> (i * 8)) & (0x000000ff);
 		sprintf(temp, "%u", ip_part_dec);
 		append_to_dstring(identifer, temp);
 
@@ -42,73 +42,6 @@ void get_connection_identifier(dstring* identifer, sa_family_t ADDRESS_FAMILY, i
 	append_to_dstring(identifer, ":");
 
 	char temp[10] = "";
-	sprintf(temp, "%u", PORT);
+	sprintf(temp, "%u", conn_grp_p->PORT);
 	append_to_dstring(identifer, temp);
-}
-
-
-// this is a global hashmap, which will store a mapping from
-// [ tcp or udp or any other protocol ] + [ip_address of the machine] + [port on the machine to talk to] -> queue_shared among the connections for the transactions 
-// effectively dstring* vs queue* (queue of job*)
-hashmap* connection_to_transaction_queue = NULL;
-rwlock* connection_access_protector = NULL;
-
-typedef struct protected_transaction_queue protected_transaction_queue;
-struct protected_transaction_queue
-{
-	queue* transaction_queue;
-	rwlock* transaction_queue_lock;
-};
-
-protected_transaction_queue* get_protected_transaction_queue()
-{
-	
-	return NULL;
-}
-
-void queue_transaction(protected_transaction_queue* pt_queue, dstring* transaction)
-{
-	// todo
-}
-
-void delete_protected_transaction_queue(protected_transaction_queue* pt_queue)
-{
-	// todo
-}
-
-
-
-
-// below are the implementation to the exposed functions, that can be used to actually create connections, add transactions to the connection queues and close the connections
-
-void intialize_transaction_client()
-{
-	if(connection_to_transaction_queue == NULL)
-	{
-		connection_to_transaction_queue = get_hashmap(10, (unsigned long long int (*)(const void*))(connection_hash), (int (*)(const void* key1, const void* key2))(compare_dstring), ELEMENTS_AS_RED_BLACK_BST);
-		connection_access_protector = get_rwlock();
-	}
-}
-
-void add_connections(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, int number_of_connections)
-{
-	dstring* url_key = get_dstring("", 10);
-	get_connection_identifier(url_key, ADDRESS_FAMILY, TRANSMISSION_PROTOCOL_TYPE, SERVER_ADDRESS, PORT);
-
-	// todo
-}
-
-void add_transaction_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT, void (*transaction)(int fd))
-{
-	// todo
-}
-
-void close_all_connections_to(sa_family_t ADDRESS_FAMILY, int TRANSMISSION_PROTOCOL_TYPE, uint32_t SERVER_ADDRESS, uint16_t PORT)
-{
-	// todo
-}
-
-void destroy_transaction_client()
-{
-	// todo
 }

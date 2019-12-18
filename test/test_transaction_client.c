4fa52d565096c252d15943cdad2dcaefb21a69cd
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-12-18 23:15:27 +0100

    increased thread count and the bug was mentioning the port no wrong

--- test/test_transaction_client.c
+++ test/test_transaction_client.c
@@ -6,7 +6,7 @@ int main()
 {
 	executor* transaction_executor = get_executor(FIXED_THREAD_COUNT_EXECUTOR, 3, -1);
 
-	connection_group* conn_grp_p = get_connection_group(AF_INET, SOCK_STREAM, 0x7f000001, 80, transaction_executor);
+	connection_group* conn_grp_p = get_connection_group(AF_INET, SOCK_STREAM, 0x7f000001, 6969, transaction_executor);
 
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("hello\r\n", 10));
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("ping\r\n", 10));
@@ -15,7 +15,10 @@ int main()
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("hey !! man\r\n", 10));
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("This is main\r\n", 10));
 
+	shutdown_executor(transaction_executor, 0);
+
 	wait_for_all_threads_to_complete(transaction_executor);
+
 	delete_executor(transaction_executor);
 
 	delete_connection_group(conn_grp_p);

c7d54ef9f4e4c209aa1ab037db8af2ad48d1ede8
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-12-18 22:58:15 +0100

    added functionality of sending message

--- test/test_transaction_client.c
+++ test/test_transaction_client.c
@@ -15,8 +15,7 @@ int main()
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("hey !! man\r\n", 10));
 	add_transaction_to_connection_group(conn_grp_p, transaction, get_dstring("This is main\r\n", 10));
 
-	shutdown_executor(transaction_executor, 0);
-
+	wait_for_all_threads_to_complete(transaction_executor);
 	delete_executor(transaction_executor);
 
 	delete_connection_group(conn_grp_p);
@@ -28,7 +27,23 @@ int transaction(int fd, void* additional_params)
 {
 	dstring* data_to_send = (dstring*) additional_params;
 
+	// send data that needs to be sent
+	int buffsentlength = send(fd, data_to_send->cstring, data_to_send->bytes_occupied, 0);
+	if(buffsentlength == -1 || buffsentlength == 0)
+	{
+		return -1;
+	}
 
+	// read the data back, as a response
+	char buffer[1000];
+	int buffreadlength = recv(fd, buffer, 999, 0);
+	if(buffreadlength == -1 || buffreadlength == 0)
+	{
+		return -1;
+	}
+	buffer[buffreadlength] = '\0';
+
+	printf("server : %s\n", buffer);
 
 	// delete additional_params
 	delete_dstring(data_to_send);

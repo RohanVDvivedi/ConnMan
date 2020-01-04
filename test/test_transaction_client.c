95e5dc4ef43a3c6cafcfd3e0d5529ce4fcbdd6da
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-05 00:15:52 +0100

    completed test code that supports transaction client in the promised way;2C

--- test/test_transaction_client.c
+++ test/test_transaction_client.c
@@ -1,6 +1,6 @@
 #include<transaction_client.h>
 
-int transaction(int fd, void* additional_params);
+void* transaction(int fd, int* close_connection_requested, void* additional_params);
 
 int main()
 {
@@ -15,46 +15,48 @@ int main()
 	job* job_5 = queue_transaction(tcli, transaction, get_dstring("hey !! man\r\n", 10));
 	job* job_6 = queue_transaction(tcli, transaction, get_dstring("This is main\r\n", 10));
 
+	printf("transactions queued successfully\n");
+
 	dstring* output = NULL;
 	dstring* input = NULL;
 
 	output = get_result_for_transaction(job_1, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job1-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
 	output = NULL;
 
 	output = get_result_for_transaction(job_2, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job2-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
 	output = NULL;
 
 	output = get_result_for_transaction(job_3, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job3-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
 	output = NULL;
 
 	output = get_result_for_transaction(job_4, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job4-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
 	output = NULL;
 
 	output = get_result_for_transaction(job_5, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job5-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
 	output = NULL;
 
 	output = get_result_for_transaction(job_6, (void**)&input);
-	printf("-> ");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
+	printf("job6-> \n");display_dstring(input);printf(" : ");display_dstring(input);printf("\n");
 	delete_dstring(input);
 	input = NULL;
 	delete_dstring(output);
@@ -68,7 +70,7 @@ int main()
 	return 0;
 }
 
-int transaction(int fd, void* additional_params)
+void* transaction(int fd, int* close_connection_requested, void* additional_params)
 {
 	dstring* data_to_send = (dstring*) additional_params;
 
@@ -76,7 +78,8 @@ int transaction(int fd, void* additional_params)
 	int buffsentlength = send(fd, data_to_send->cstring, data_to_send->bytes_occupied, 0);
 	if(buffsentlength == -1 || buffsentlength == 0)
 	{
-		return -1;
+		(*close_connection_requested) = 1;
+		return get_dstring("ERROR", 10);
 	}
 
 	// read the data back, as a response
@@ -84,15 +87,13 @@ int transaction(int fd, void* additional_params)
 	int buffreadlength = recv(fd, buffer, 999, 0);
 	if(buffreadlength == -1 || buffreadlength == 0)
 	{
-		return -1;
+		(*close_connection_requested) = 1;
+		return get_dstring("ERROR", 10);
 	}
 	buffer[buffreadlength] = '\0';
 
-	printf("server : %s\n", buffer);
+	printf("job_completed\n");
 
-	// delete additional_params
-	delete_dstring(data_to_send);
-
-	return 0;
+	return get_dstring(buffer, 10);
 }
 

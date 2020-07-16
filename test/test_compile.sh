1f3f78d1c561bef417e28935207dd77c1f6ac1c0
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-16 22:17:30 +0530

    removed redundant lines from test compile script

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,7 +1,3 @@
-echo "compiling server"
 gcc test_server.c -o server.out -lpthread -lconnman -lboompar -lcutlery
-echo "compiling client"
 gcc test_client.c -o client.out -lpthread -lconnman -lboompar -lcutlery
-echo "compiling transaction_client"
 gcc test_transaction_client.c -o tclient.out -lpthread -lconnman -lboompar -lcutlery
-echo "compiling done"

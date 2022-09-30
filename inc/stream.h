dc9de67e29cd6dd8471261a0786b0712ccc5893f
RohanVDvivedi <rohandvivedi@gmail.com>
2022-09-30 13:00:21 -0700

    started implementing stream interface

--- inc/stream.h
+++ inc/stream.h
@@ -1,22 +1,40 @@
 #ifndef STREAM_H
 #define STREAM_H
 
-typedef struct read_stream read_stream;
-struct read_stream
+#include<dpipe.h>
+
+typedef struct stream stream;
+struct stream
 {
 	void* stream_context;
+
+	// this pipe stores data that was unread_to_stream
+	dpipe unread_data;
 
 	// returns bytes read from data, (atmost data_size number of bytes will be touched)
-	unsigned int (*read_from_stream)(void* stream_context, void* data, unsigned int data_size);
-};
-
-typedef struct write_stream write_stream;
-struct write_stream
-{
-	void* stream_context;
+	unsigned int (*read_from_stream_context)(void* stream_context, void* data, unsigned int data_size, int* error);
 
 	// returns bytes written from data (that consists of data_size number of bytes to be written)
-	unsigned int (*write_to_stream)(void* stream_context, const void* data, unsigned int data_size);
+	unsigned int (*write_to_stream_context)(void* stream_context, const void* data, unsigned int data_size, int* error);
+
+	int error;
 };
+
+void initialize_stream(stream* strm, void* stream_context, unsigned int (*read_from_stream_context)(void* stream_context, void* data, unsigned int data_size, int* error), unsigned int (*write_to_stream_context)(void* stream_context, const void* data, unsigned int data_size, int* error));
+
+int is_readable_stream(stream* strm);
+
+int is_writable_stream(stream* strm);
+
+int get_error_stream(stream* strm);
+
+unsigned int read_from_stream(stream* rs, void* data, unsigned int data_size);
+
+// returns 1 on success else a 0
+int unread_from_stream(stream* rs, const void* data, unsigned int data_size);
+
+unsigned int write_to_stream(stream* ws, const void* data, unsigned int data_size);
+
+void deinitialize_stream(stream* strm);
 
 #endif
---
+++ src/stream.c
@@ -0,0 +1,78 @@
+#include<stream.h>
+
+#include<stddef.h>
+
+void initialize_stream(stream* strm, void* stream_context, unsigned int (*read_from_stream_context)(void* stream_context, void* data, unsigned int data_size, int* error), unsigned int (*write_to_stream_context)(void* stream_context, const void* data, unsigned int data_size, int* error))
+{
+	strm->stream_context = stream_context;
+	initialize_dpipe(&(strm->unread_data), 0);
+	strm->read_from_stream_context = read_from_stream_context;
+	strm->write_to_stream_context = write_to_stream_context;
+	strm->error = 0;
+}
+
+int is_readable_stream(stream* strm)
+{
+	return strm->read_from_stream_context != NULL;
+}
+
+int is_writable_stream(stream* strm)
+{
+	return strm->write_to_stream_context != NULL;
+}
+
+int get_error_stream(stream* strm)
+{
+	return strm->error;
+}
+
+unsigned int read_from_stream(stream* strm, void* data, unsigned int data_size)
+{
+	if(strm->read_from_stream_context == NULL)
+		return 0;
+
+	unsigned int bytes_read = read_from_dpipe(&(strm->unread_data), data, data_size, PARTIAL_ALLOWED);
+
+	if(bytes_read < data_size)
+		bytes_read += strm->read_from_stream_context(strm->stream_context, data + bytes_read, data_size - bytes_read, &(strm->error));
+
+	return bytes_read;
+}
+
+int unread_from_stream(stream* strm, const void* data, unsigned int data_size)
+{
+	if(strm->read_from_stream_context == NULL)
+		return 0;
+
+	unsigned int bytes_unread = unread_to_dpipe(&(strm->unread_data), data, data_size, ALL_OR_NONE);
+
+	if(bytes_unread == 0)
+	{
+		resize_dpipe(&(strm->unread_data), get_capacity_dpipe(&(strm->unread_data)) + data_size + 1024);
+		bytes_unread = unread_to_dpipe(&(strm->unread_data), data, data_size, ALL_OR_NONE);
+	}
+
+	return bytes_unread == data_size;
+}
+
+unsigned int write_to_stream(stream* strm, const void* data, unsigned int data_size)
+{
+	if(strm->write_to_stream_context == NULL)
+		return 0;
+
+	unsigned int bytes_written = 0;
+	while(bytes_written < data_size)
+	{
+		bytes_written += strm->write_to_stream_context(strm->stream_context, data + bytes_written, data_size - bytes_written, &(strm->error));
+
+		if(strm->error)
+			break;
+	}
+	return bytes_written;
+}
+
+void deinitialize_stream(stream* strm)
+{
+	deinitialize_dpipe(&(strm->unread_data));
+	*strm = (stream){};
+}

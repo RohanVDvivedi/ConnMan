5c43bc7c39fedc5ae54f50b8589433eaccef2550
RohanVDvivedi <rohandvivedi@gmail.com>
2022-10-01 12:28:44 -0700

    added declaration for client stream connection

--- inc/client.h
+++ inc/client.h
@@ -8,6 +8,11 @@
 // here, client_addr_p is optional, it can be used to connect to a server using a fixed port or ip
 int make_connection(comm_address* server_addr_p, comm_address* client_addr_p);
 
-
+// below function initializes a new stream connecting to server_addr_p
+// if ssl_ctx is not NULL, then a ssl stream is returned
+// it returns 1 for success, else it returns -1 for an error
+// you can close the connection by close_stream(strm), after you car done with the connection
+// this function sould only be used with tcp connections
+int make_connection_stream(stream* strm, comm_address* server_addr_p, comm_address* client_addr_p, SSL_CTX* ssl_ctx);
 
 #endif

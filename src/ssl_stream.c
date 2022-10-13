8a9aea36da6c370f576fa262e356f70b426881dc
RohanVDvivedi <rohandvivedi@gmail.com>
2022-10-13 15:34:37 -0700

    using fd from get_fd() from ssl to close the stream

--- src/ssl_stream.c
+++ src/ssl_stream.c
@@ -3,20 +3,14 @@
 #include<unistd.h>
 #include<errno.h>
 
-typedef struct ssl_stream_context ssl_stream_context;
-struct ssl_stream_context
-{
-	int fd;
-	SSL* ssl;
-};
-
 static unsigned int read_from_ssl(void* ssl_sc_vp, void* data, unsigned int data_size, int* error)
 {
-	ssl_stream_context* ssl_sc = ssl_sc_vp;
-	int ret = SSL_read(ssl_sc->ssl, data, data_size);
+	SSL* ssl = ssl_sc_vp;
+
+	int ret = SSL_read(ssl, data, data_size);
 	if(ret <= 0)
 	{
-		*error = SSL_get_error(ssl_sc->ssl, ret);
+		*error = SSL_get_error(ssl, ret);
 		return 0;
 	}
 	return ret;
@@ -24,12 +18,12 @@ static unsigned int read_from_ssl(void* ssl_sc_vp, void* data, unsigned int data
 
 static unsigned int write_to_ssl(void* ssl_sc_vp, const void* data, unsigned int data_size, int* error)
 {
-	ssl_stream_context* ssl_sc = ssl_sc_vp;
+	SSL* ssl = ssl_sc_vp;
 
-	int ret = SSL_write(ssl_sc->ssl, data, data_size);
+	int ret = SSL_write(ssl, data, data_size);
 	if(ret <= 0)
 	{
-		*error = SSL_get_error(ssl_sc->ssl, ret);
+		*error = SSL_get_error(ssl, ret);
 		return 0;
 	}
 	return ret;
@@ -37,62 +31,55 @@ static unsigned int write_to_ssl(void* ssl_sc_vp, const void* data, unsigned int
 
 static void close_stream_context_ssl(void* ssl_sc_vp, int* error)
 {
-	ssl_stream_context* ssl_sc = ssl_sc_vp;
+	SSL* ssl = ssl_sc_vp;
+	int fd = SSL_get_fd(ssl);
 
-	int ret = SSL_shutdown(ssl_sc->ssl);
+	int ret = SSL_shutdown(ssl);
 	if(ret < 0)
-		*error = SSL_get_error(ssl_sc->ssl, ret);
+		*error = SSL_get_error(ssl, ret);
 
-	ret = close(ssl_sc->fd);
+	ret = close(fd);
 	if(ret == -1)
 		*error = errno;
 }
 
 static void destroy_stream_context_ssl(void* ssl_sc_vp)
 {
-	ssl_stream_context* ssl_sc = ssl_sc_vp;
-	SSL_free(ssl_sc->ssl);
-	free(ssl_sc);
+	SSL* ssl = ssl_sc_vp;
+	
+	SSL_free(ssl);
 }
 
 int initialize_stream_for_ssl_server(stream* strm, SSL_CTX* ctx, int fd)
 {
-	ssl_stream_context* ssl_sc = malloc(sizeof(ssl_stream_context));
+	SSL* ssl = SSL_new(ctx);
+	SSL_set_fd(ssl, fd);
 
-	ssl_sc->fd = fd;
-
-	ssl_sc->ssl = SSL_new(ctx);
-	SSL_set_fd(ssl_sc->ssl, fd);
-
-	SSL_set_accept_state(ssl_sc->ssl);
-	int ret = SSL_accept(ssl_sc->ssl);
+	SSL_set_accept_state(ssl);
+	int ret = SSL_accept(ssl);
 	if(ret <= 0)
 		return 0;
 
-	SSL_set_mode(ssl_sc->ssl, SSL_MODE_AUTO_RETRY);
+	SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
 
-	initialize_stream(strm, ssl_sc->ssl, read_from_ssl, write_to_ssl, close_stream_context_ssl, destroy_stream_context_ssl);
+	initialize_stream(strm, ssl, read_from_ssl, write_to_ssl, close_stream_context_ssl, destroy_stream_context_ssl);
 
 	return 1;
 }
 
 int initialize_stream_for_ssl_client(stream* strm, SSL_CTX* ctx, int fd)
 {
-	ssl_stream_context* ssl_sc = malloc(sizeof(ssl_stream_context));
+	SSL* ssl = SSL_new(ctx);
+	SSL_set_fd(ssl, fd);
 
-	ssl_sc->fd = fd;
-
-	ssl_sc->ssl = SSL_new(ctx);
-	SSL_set_fd(ssl_sc->ssl, fd);
-
-	SSL_set_connect_state(ssl_sc->ssl);
-	int ret = SSL_connect(ssl_sc->ssl);
+	SSL_set_connect_state(ssl);
+	int ret = SSL_connect(ssl);
 	if(ret <= 0)
 		return 0;
 
-	SSL_set_mode(ssl_sc->ssl, SSL_MODE_AUTO_RETRY);
+	SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
 
-	initialize_stream(strm, ssl_sc, read_from_ssl, write_to_ssl, close_stream_context_ssl, destroy_stream_context_ssl);
+	initialize_stream(strm, ssl, read_from_ssl, write_to_ssl, close_stream_context_ssl, destroy_stream_context_ssl);
 
 	return 1;
 }

f3bfa3b0cc89141eb12303b71156c5499505a950
RohanVDvivedi <rohandvivedi@gmail.com>
2022-10-17 14:13:02 -0700

    added a comment command to generate a self signed key and cert file for testing

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -3,3 +3,6 @@ gcc test_client.c -o client.out -lconnman -lboompar -lpthread -lcutlery -lssl -l
 
 gcc test_stream_server.c -o stream_server.out -lconnman -lboompar -lpthread -lcutlery -lssl -lcrypto
 gcc test_stream_client.c -o stream_client.out -lconnman -lboompar -lpthread -lcutlery -lssl -lcrypto
+
+# use below command to generate a self signed certificate
+#openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365

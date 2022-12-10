b8e495e35231825f55d04bf6adc8c6b7ee0dc561
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-10 13:20:59 -0700

    started with reimplementation of basic functions

--- src/client_set.c
+++ src/client_set.c
@@ -27,10 +27,40 @@ client_set* new_client_set(const comm_address* server_addr_p, SSL_CTX* ssl_ctx, 
 
 unsigned int get_max_clients(client_set* cls)
 {
+	pthread_mutex_lock(&(cls->client_set_lock));
+	unsigned int max_clients = cls->max_client_count;
+	pthread_mutex_unlock(&(cls->client_set_lock));
+	return max_clients;
 }
 
 int reset_max_clients(client_set* cls, unsigned int max_clients)
 {
+	if(max_clients == 0)
+		max_clients = 1;
+
+	int reset_max_clients_success = 0;
+
+	pthread_mutex_lock(&(cls->client_set_lock));
+
+	if(!cls->shutdown_called)
+	{
+		if(max_clients < cls->max_client_count)
+		{
+			
+			// mark return value as success
+			reset_max_clients_success = 1;
+		}
+		else if(max_clients > cls->max_client_count)
+		{
+
+			// mark return value as success
+			reset_max_clients_success = 1;
+		}
+	}
+
+	pthread_mutex_unlock(&(cls->client_set_lock));
+
+	return reset_max_clients_success;
 }
 
 stream* reserve_client(client_set* cls)

6362f5fbe9aa681dc8a0e8e64bc14061c83d2e89
RohanVDvivedi <rohandvivedi@gmail.com>
2022-12-12 15:39:20 -0700

    started with implementing a writable dstring stream

---
+++ inc/writable_dstring_stream.h
@@ -0,0 +1,8 @@
+#ifndef WRITABLE_DSTRING_STREAM_H
+#define WRITABLE_DSTRING_STREAM_H
+
+#include<stream.h>
+
+void initialize_writable_dstring_stream(stream* strm);
+
+#endif
---
+++ src/writable_dstring_stream.c
@@ -0,0 +1,6 @@
+#include<writable_dstring_stream.h>
+
+void initialize_writable_dstring_stream(stream* strm)
+{
+	
+}

fe762217ae70f952699d1ac6dc473dffb5e74cfd
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-05 14:52:17 -0700

    write formatted will take care of all writes

--- inc/stream_util.h
+++ inc/stream_util.h
@@ -5,12 +5,6 @@
 
 #include<stream.h>
 
-unsigned int write_uint64_to_stream(stream* ws, uint64_t u);
-
-unsigned int write_int64_to_stream(stream* ws, int64_t i);
-
-unsigned int write_dstring_to_stream(stream* ws, const dstring* str);
-
-unsigned int write_cstring_to_stream(stream* ws, const char* str);
+unsigned int write_to_stream_formatted(stream* ws, );
 
 #endif

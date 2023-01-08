4c99a922b5e365155927fcaba07ac7770e2c7438
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-07 21:40:47 -0700

    implemented test cases for stream util

--- test/test_stream_util.c
+++ test/test_stream_util.c
@@ -1,7 +1,8 @@
-#include<stream.h>
 #include<stream_util.h>
+#include<file_descriptor_stream.h>
 
 #include<stdio.h>
+#include<inttypes.h>
 
 int main()
 {
@@ -11,7 +12,37 @@ int main()
 
 	int error = 0;
 
-	write_to_stream_formatted(&ws, "%s %d %c %u %f", &error, "Rohan", 123, 'X', 123.123);
+	write_to_stream_formatted(&ws, "%s %d %c %u %f\n", &error, "Rohan", 123, 'X', 123.123);
+
+	if(error)
+	{
+		printf("error encountered white writing to output stream\n");
+		return 0;
+	}
+
+	printf("enter space and then a uint64_t number\n");
+
+	unsigned int spaces_skipped = skip_whitespaces_from_stream(&rs, 16, &error);
+	if(error)
+	{
+		printf("error skipping whitespaces\n");
+		return 0;
+	}
+	printf("%u whitespaces were skipped\n", spaces_skipped);
+
+	uint64_t num = 0;
+	unsigned int bytes_read = read_uint64_from_stream(&rs, &num, &error);
+	printf("%u bytes read trying to read a number\n", bytes_read);
+	if(error)
+	{
+		printf("error reading uint64_t number\n");
+		return 0;
+	}
+	printf("%"PRIu64 " number was read\n", num);
+
+
+	deinitialize_stream(&rs);
+	deinitialize_stream(&ws);
 
 	return 0;
 }

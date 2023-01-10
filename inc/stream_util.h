c060c67d79bd25217cbd3be11aabdde348179611
RohanVDvivedi <rohandvivedi@gmail.com>
2023-01-10 13:48:21 -0700

    added declaration for read_dstring_until from_stream

--- inc/stream_util.h
+++ inc/stream_util.h
@@ -16,4 +16,11 @@ unsigned int skip_whitespaces_from_stream(stream* rs, unsigned int max_whitespac
 // if return is non zero then an instance of str_to_skip was encountered
 unsigned int skip_dstring_from_stream(stream* rs, const dstring* str_to_skip, int* error);
 
+// bytes will be read until the until_str dstring is encountered in the stream
+// all bytes read from the stream will be retunred in the return dstring
+// on success (if until dstring is encountered) the returned dstring will also contain until_str as its suffix
+// on failure (if max_bytes_to_read is encountered OR the stream is closed) then an empty dstring will be returned
+// in any case, no more than max_bytes_to_read bytes will be read
+dstring read_dstring_until_from_stream(stream* rs, const dstring* until, unsigned int max_bytes_to_read, int* error);
+
 #endif

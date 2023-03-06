def9760def4f8ecb10639b4c25b49469a4c53287
RohanVDvivedi <rohandvivedi@gmail.com>
2023-03-05 21:40:11 -0700

    implemented intialize and deinitialize functions for stacked stream

--- src/stacked_stream.c
+++ src/stacked_stream.c
@@ -3,5 +3,17 @@
 // the (read_streams and write_streams) stacks in the stacked_stream are initialized with capacity equal to DEFAULT_STACKED_STREAM_STACKS_CAPACITY
 #define DEFAULT_STACKED_STREAM_STACKS_CAPACITY 4
 
+void initialize_stacked_stream(stacked_stream* sstrm)
+{
+	initialize_stack(&(sstrm->read_streams), DEFAULT_STACKED_STREAM_STACKS_CAPACITY);
+	initialize_stack(&(sstrm->write_streams), DEFAULT_STACKED_STREAM_STACKS_CAPACITY);
+}
+
+void deinitialize_stacked_stream(stacked_stream* sstrm)
+{
+	deinitialize_stack(&(sstrm->read_streams));
+	deinitialize_stack(&(sstrm->write_streams));
+}
+
 // all the bits that must be 0 for the operate_on parameter
 #define NOT_OPERATE_ON_MASK (~BOTH_STREAMS)

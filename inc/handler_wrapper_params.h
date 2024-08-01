58a8820be7619dff4a8bfbf8553a90c05a2cd815
RohanVDvivedi <rohandvivedi@gmail.com>
2024-08-01 22:24:37 +0530

    malloc is assumed to may fail

--- inc/handler_wrapper_params.h
+++ inc/handler_wrapper_params.h
@@ -14,6 +14,8 @@ struct handler_wrapper_input_params
 static handler_wrapper_input_params* new_handler_wrapper_input_params(int fd, void* additional_params, void (*handler)(int fd, void* additional_params))
 {
 	handler_wrapper_input_params* handler_wrapper_input_params_p = (handler_wrapper_input_params*)malloc(sizeof(handler_wrapper_input_params));
+	if(handler_wrapper_input_params_p == NULL)
+		return NULL;
 	handler_wrapper_input_params_p->fd = fd;
 	handler_wrapper_input_params_p->additional_params = additional_params;
 	handler_wrapper_input_params_p->handler = handler;
--- inc/stream_handler_wrapper_params.h
+++ inc/stream_handler_wrapper_params.h
@@ -17,6 +17,8 @@ struct stream_handler_wrapper_input_params
 static stream_handler_wrapper_input_params* new_stream_handler_wrapper_input_params(int fd, SSL_CTX* ssl_ctx, void* additional_params, void (*stream_handler)(stream* strm, void* additional_params))
 {
 	stream_handler_wrapper_input_params* stream_handler_wrapper_input_params_p = (stream_handler_wrapper_input_params*)malloc(sizeof(stream_handler_wrapper_input_params));
+	if(stream_handler_wrapper_input_params_p == NULL)
+		return NULL;
 	stream_handler_wrapper_input_params_p->fd = fd;
 	stream_handler_wrapper_input_params_p->ssl_ctx = ssl_ctx;
 	stream_handler_wrapper_input_params_p->additional_params = additional_params;

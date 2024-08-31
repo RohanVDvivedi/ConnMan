a6ad5ebba9d33713c97aee21b22882b679f68d31
RohanVDvivedi <rohandvivedi@gmail.com>
2024-08-31 23:43:20 +0530

    moved a lot of stream functionality to Cutlery, relieving burden of the ConnMan

--- inc/file_descriptor_stream.h
+++
@@ -1,8 +0,0 @@
-#ifndef FILE_DESCRIPTOR_STREAM
-#define FILE_DESCRIPTOR_STREAM
-
-#include<stream.h>
-
-int initialize_stream_for_fd(stream* strm, int fd);
-
-#endif
--- inc/writable_dstring_stream.h
+++
@@ -1,9 +0,0 @@
-#ifndef WRITABLE_DSTRING_STREAM_H
-#define WRITABLE_DSTRING_STREAM_H
-
-#include<stream.h>
-#include<dstring.h>
-
-int initialize_writable_dstring_stream(stream* strm, dstring* write_to);
-
-#endif
